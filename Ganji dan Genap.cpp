f98dd31369ad09edd1b3bdcd603540dc1ad4f6e9
Riski Abiyantoro <56922497+riski-abiyan@users.noreply.github.com>
2019-10-26 19:37:38 +0700

    Update and rename README.md to Ganji dan Genap.cpp

---
+++ Ganji dan Genap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int angka;
+	
+	cout<<"Program menentukan Ganjil atau Genap"<<endl;
+	cout<<endl;
+	cout<<"Masukan Angka 1-9 :"<<endl;
+	cin>>angka;
+	
+	system ("cls"); {
+		
+		cout<<"Hasilnya adalah :"<<endl;
+		cout<<endl;
+	
+	if (angka % 2 == 0 && angka-0 == angka)
+	{
+		cout<<"-----angka yang anda masukan Genap-----";
+	}
+    else 
+	{
+		cout<<"-----angka yang anda masukan Ganjil-----";
+	}
+	
+	}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# C-
-All c++ program 
