2786346184d1fa239956ea53c48f09bbd63531e2
Riski Abiyantoro <56922497+riski-abiyan@users.noreply.github.com>
2019-10-26 19:39:58 +0700

    Create Increment kelipatan 5.cpp

---
+++ Increment kelipatan 5.cpp
@@ -0,0 +1,15 @@
+
+//Nomor 1
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a;
+	
+	cout << "Bilangan Kelipatan 5 Antara 125 - 200" << endl;
+	
+	for ( a=125; a<=200; a+=5 ) {
+		cout << a <<" ";
+	}
+}
+
